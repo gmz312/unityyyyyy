b61107f8ee71412a5543637cf9a5a08b778b7b46
2040196969@qq.com <2040196969@qq.com>
2023-01-12 13:43:59 +0800

    粒子系统

---
+++ unity大作业项目——粒子系统/Assets/UserGUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour
+{
+
+    public flare flare1, flare2, flare3,flare4, flare5;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 150, 60, 30), "向左倾斜"))
+        {
+            flare1.LeftWind();
+            flare2.LeftWind();
+            flare3.LeftWind();
+            flare4.LeftWind();
+            flare5.LeftWind();
+        }
+
+        if (GUI.Button(new Rect(Screen.width-60, 150, 60, 30), "向右倾斜"))
+        {
+            flare1.RightWind();
+            flare2.RightWind();
+            flare3.RightWind();
+            flare4.RightWind();
+            flare5.RightWind();
+        }
+    }
+}
---
+++ unity大作业项目——粒子系统/Assets/flare.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flare : MonoBehaviour
+{
+    ParticleSystem particleSystem;
+    ParticleSystem.ForceOverLifetimeModule forceMode;
+    ParticleSystem.ColorOverLifetimeModule colorMode;
+    int windPower;
+
+    // Use this for initialization
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        forceMode = particleSystem.forceOverLifetime;
+        colorMode = particleSystem.colorOverLifetime;
+    }
+
+    public void LeftWind()
+    {
+        ParticleSystem.MinMaxCurve temp = forceMode.x;
+        temp.constantMax += -0.3f;
+        forceMode.x = temp;
+    }
+
+    public void RightWind()
+    {
+        ParticleSystem.MinMaxCurve temp = forceMode.x;
+        temp.constantMax += 0.3f;
+        forceMode.x = temp;
+    }
+}
